d6dabfabe3223f9b582698614834d0f9be214794
Homval <in00@list.ru>
2024-08-04 08:26:45 +0300

    add edit modal in admin panel

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRoleController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminRoleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.entities.Role;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.services.RoleServiceInterface;
 import ru.kata.spring.boot_security.demo.services.UserServiceInterface;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
@@ -27,9 +29,11 @@ public class AdminRoleController {
     @GetMapping
     public String userList(Principal principal, Model model) {
         User admin = userService.findByUsername(principal.getName());
+        List<Role> roles = roleService.getRoles();
         List<User> users = userService.findAllUsers();
         model.addAttribute("users", users);
         model.addAttribute("admin", admin);
+        model.addAttribute("roles", roles);
         return "/admin/admin";
     }
 
@@ -52,17 +56,17 @@ public class AdminRoleController {
         return "redirect:/admin";
     }
 
-    @GetMapping("/edit")
-    public String editUser(@RequestParam(name = "id") long id, Model model) {
-        User user = userService.findById(id);
-        model.addAttribute("user", user);
-        model.addAttribute("roles", roleService.getRoles());
-        return "/admin/edit_user";
-    }
+//    @GetMapping("/edit")
+//    public String editUser(@RequestParam(name = "id") long id, Model model) {
+//        User user = userService.findById(id);
+//        model.addAttribute("user", user);
+//        model.addAttribute("roles", roleService.getRoles());
+//        return "/admin/edit_user";
+//    }
 
     @PostMapping("/edit")
     public String updateUser(@ModelAttribute("user") User user) {
         userService.save(user);
-        return "redirect:/admin";
+        return "redirect:/admin/admin";
     }
 }
